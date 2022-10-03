b04593ed7978f44d212af3c4ece7fd54adbca8ff
carlo <carlo@LAPTOP-TVUIK7SM>
2022-10-03 20:42:44 +0200

    Merge origin/master
    Conflicts:
    	src/civitas/Civitas.java

--- src/civitas/Civitas.java
+++ src/civitas/Civitas.java
@@ -4,50 +4,89 @@
  */
 package civitas;
 import civitas.Casilla.TipoCasilla;
-/**
- *
- * @author LG
- */
+
 public class Civitas {
-    
-    /**
-     * @param args the command line arguments
-     */
-    
+
    
     public static void main(String[] args) {
-        // TODO code application logic here
+    /*    
+       for(int i = 0; i < 100; i++){
+           
+          System.out.println(Dado.getInstance().quienEmpieza(4));
+           
+       }
+     */  
+       Dado.getInstance().setDebug(true);
+       
+       for(int i = 0; i < 10; i++)
+           System.out.println(Dado.getInstance().tirar());
         
-        int tiradas[]={0,0,0,0};
+       Dado.getInstance().setDebug(false);
+       
+       for(int i = 0; i < 10; i++)
+           System.out.println(Dado.getInstance().tirar());
+       
+        System.out.println("El dado esta en " + 
+                Dado.getInstance().getUltimoResultado());
         
-        Dado dadito=Dado.getInstance();
         
-        dadito.setDebug(false);
+        System.out.println("Tipos enumerados: \n"
+                + TipoCasilla.CALLE + "\n"      
+                + TipoSorpresa.PAGARPORCOBRAR);
         
-        for(int i=0; i<100; i++)
-        {
-            int numero=dadito.quienEmpieza(4);
         
-            if (numero==1) tiradas[0]++;
-            if (numero==2) tiradas[1]++;
-            if (numero==3) tiradas[2]++;
-            if (numero==4) tiradas[3]++;
-                
-        }
+        Tablero tab = new Tablero();
         
-        for(int i =0; i<4; i++)
-        {
-            System.out.println(tiradas[i]);
-        }
+        Casilla cas = new Casilla(TipoCasilla.CALLE, "calle1", 0, 0, 0);
+        Casilla cas2 = new Casilla(TipoCasilla.CALLE, "calle2", 500, 0, 0);
+        Casilla cas3 = new Casilla(TipoCasilla.CALLE, "calle3", 1000, 0, 0);
         
-        for(int i =0; i<4; i++)
-        {
-            System.out.println(dadito.tirar());
-            System.out.println("pro");
-            System.out.println("Cebollas2");
+        tab.añadeCasilla(cas);
+        tab.añadeCasilla(cas2);
+        tab.añadeCasilla(cas3);
+        
+        System.out.println(tab.getCasilla(1).toString());
+        System.out.println(tab.getCasilla(2).toString());
+        System.out.println(tab.getCasilla(3).toString());
+        
+        Casilla masCara, masBarata;
+        
+        if(cas.getPrecioCompra() > cas2.getPrecioCompra()){
+            masCara = cas;
+            
+            masBarata = cas2;
+            
+            if(cas3.getPrecioCompra() > cas.getPrecioCompra())
+                masCara = cas3;
+            else if (cas3.getPrecioCompra() < cas2.getPrecioCompra())
+                masBarata = cas3;
+            
+        }else{
+            
+            masCara = cas2;
+            
+            masBarata = cas;
+            
+            if(cas3.getPrecioCompra() > cas2.getPrecioCompra())
+                masCara = cas3;
+            else if (cas3.getPrecioCompra() < cas.getPrecioCompra())
+                masBarata = cas3;
             
         }
+        
+        float precioMedio = (cas.getPrecioCompra() + 
+                            cas2.getPrecioCompra() +
+                            cas3.getPrecioCompra())/3;
+        
+        System.out.println("La casilla mas cara es " + masCara + 
+                            " y las calles tienen un precio medio de " + 
+                            precioMedio);
+        
+        Diario.getInstance().ocurreEvento("Un jugador ha ganado la partida");
+        
+        for(int i = 0; i < 3; i++){
+            System.out.println("La posicion final es: " + tab.nuevaPosicion(i, Dado.getInstance().tirar()));
+        }
     }
-    
     
 }
