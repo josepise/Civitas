7d988019ca186ad7e360f6e89778f6035f6c4970
carlo <carlo@LAPTOP-TVUIK7SM>
2022-10-03 20:35:45 +0200

    Merge origin/master
    Conflicts:
    	src/civitas/Civitas.java

--- src/civitas/Civitas.java
+++ src/civitas/Civitas.java
@@ -45,9 +45,7 @@ public class Civitas {
             System.out.println(dadito.tirar());
             System.out.println("pro");
             System.out.println("Cebollas2");
-            System.out.println("po");
-            System.out.println("jasdfj");
-            System.out.println("snodqw");
+            
         }
     }
     
